a1077b213376008547c4e2bc2ed167d03bc19c9c
Harvey.Wu <whw880218@pgtalk.com>
2023-07-07 16:49:27 +0800

    測試並大修改revert

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,94 @@
 <template>
-  <div class="hello">
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <h3>接到的資料:{{ showData }}</h3>
+  <div class="hello-world">
+    <button @click="fetchData">获取数据</button>
+    <button @click="oauthSignIn">登陸OAuth</button>
   </div>
 </template>
 <script>
-import axios from "axios";
+// import axios from "axios";
 export default {
   name: "HelloWorld",
   data() {
+    const CLIENT_ID = '800172214658-6gee9rsf9aa99mas1fu8dhh8jbrapikv.apps.googleusercontent.com'; // 替换为您的 OAuth 2.0 客户端 ID
+    const CLIENT_SECRET = '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'; // 替换为您的 OAuth 2.0 客户端密钥
+    const API_URL = 'https://analyticsdata.googleapis.com/v1beta/properties/380344010/metadata?key=AIzaSyCaADd3bfYV7erEvFJzjmBtcUnV6V5aTTc'; // Google Analytics 数据 API 端点
+
+    const ACCESS_TOKEN = '';
     return {
       reports: [],
+      CLIENT_ID,
+      CLIENT_SECRET,
+      API_URL,
+      ACCESS_TOKEN
     };
   },
-  mounted() {
-    this.getAnalyticsData();
-  },
   methods: {
-    async getAnalyticsData() {
-      const VIEW_ID = "380344010"; // 替换为您的 Google Analytics 视图 ID
-      const API_KEY = "AIzaSyCaADd3bfYV7erEvFJzjmBtcUnV6V5aTTc"; // 替换为您的 Google Cloud API 密钥
+    // 发起 API 请求
+    async fetchData() {
+      try {
+        let url = window.location.href
+        let urlarry = url.split('&');
+        let tokenurl = urlarry[1].split('=');
+        this.ACCESS_TOKEN = tokenurl[1];
+        const headers = {
+          Authorization: 'Bearer ' + this.ACCESS_TOKEN,
+          'Access-Control-Allow-Origin': 'http://localhost:8080' 
+        };
 
-      const response = await axios
-        .post(
-          `https://analyticsdata.googleapis.com/v1beta/123456789:runReport`,
-          {
-            reportRequests: [
-              {
-                viewId: VIEW_ID,
-                metrics: [
-                  { expression: "ga:users" },
-                  { expression: "ga:sessions" },
-                ],
-                dateRanges: [{ startDate: "10daysAgo", endDate: "today" }],
-              },
-            ],
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${API_KEY}`,
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
+        const requestBody = {
+          reportRequests: [
+            {
+              viewId: 'G-TM9FT02FW3', // 替换为您的 Google Analytics 视图 ID
+              dateRanges: [
+                {
+                  startDate: '2023-06-01',
+                  endDate: '2023-06-11',
+                },
+              ],
+              metrics: [
+                {
+                  expression: 'ga:sessions',
+                },
+              ],
+              dimensions: [
+                {
+                  name: 'ga:country',
+                },
+              ],
             },
-          }
-        )
-        .then((response) => {
-          // 处理响应数据
+          ],
+        };
+
+        await fetch(
+          this.API_URL, requestBody, {
+          method: 'POST',
+          headers: headers,
+          body: requestBody
+        }).then((response) => {
+          console.log('成功获取 Google Analytics 数据：', response.data);
           console.log(response);
-          this.reports=response;
         })
-        .catch((error) => {
-          // 处理错误
-          console.error(error);
-        });
-      console.log(response);
-      // const data = response.data;
-      // 在这里处理返回的数据
-      // console.log(data);
+          .catch((error) => {
+            console.error('获取 Google Analytics 数据失败：', error);
+          });
+      } catch (error) {
+        console.error('API 请求失败', error);
+        throw error;
+      }
     },
+    // 获取访问令牌
+    oauthSignIn() {
+      // Google's OAuth 2.0 endpoint for requesting an access token /Google OAuth 2.0 Token 端点
+      let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+      let params = new URLSearchParams();
+      params.append('client_id', '800172214658-6gee9rsf9aa99mas1fu8dhh8jbrapikv.apps.googleusercontent.com');
+      params.append('redirect_uri', 'http://localhost:8080/VGtest/');
+      params.append('response_type', 'token');
+      params.append('scope', 'https://www.googleapis.com/auth/drive.metadata.readonly');
+      params.append('include_granted_scopes', 'true');
+      params.append('state', 'pass-through value');
+      window.location.href = `${oauth2Endpoint}?${params.toString()}`;
+    }
   },
 };
 </script>
